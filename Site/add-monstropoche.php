af04da0b0455962d6879cc65cc899de7ce14f599
NAUDY Enzo <enaudy@florette.pedago.ipb.fr>
2022-03-25 11:53:16 +0100

    monstropoche

--- Site/add-monstropoche.php
+++ Site/add-monstropoche.php
@@ -11,16 +11,16 @@
 </body>
 <?php 
     include 'connect.php';
-    $newnickame = $_REQUEST['nickname'];
+    $newnickame = $_REQUEST['Nickname'];
     $state = 'Repos';
     $EXP = intval('0');
     $HP = intval($_REQUEST['HP']);
-    $gender = $_REQUEST['gender'];
-    $species = $_REQUEST['species'];
-    $object = intval($_REQUEST['object']);
-    $attack = intval($_REQUEST['attack']);
-    $owner = intval($_REQUEST['owner']);
-    $sql = 'insert into MONSTROPOCHE (surnom, etat, pe, pv , genre, espece, objet, attaque, proprietaire) values (:surnom, :etat, :pe, :pv, :genre, :espece, :objet, :proprietaire); insert into HABITAT(numEspece, IdZone) values(:number, :zone)';
+    $gender = $_REQUEST['Gender'];
+    $species = $_REQUEST['Species'];
+    $object = intval($_REQUEST['Object']);
+    $attack = intval($_REQUEST['Attack']);
+    $owner = intval($_REQUEST['Owner']);
+    $sql = 'insert into MONSTROPOCHE (surnom, etat, pe, pv , genre, espece, objet, attaque, proprietaire) values (:surnom, :etat, :pe, :pv, :genre, :espece, :objet, :proprietaire);';
     $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
     $sth->execute(array('surnom'=> $newnickame, 'etat' => $state, 'pe' => $EXP, 'pv' => $HP, 'genre' => $gender, 'espece' => $species, 'objet' => $object, 'attaque' => $attack, 'proprietaire' => $owner));
     ?>
