ad1dc5c891838c0c1a0b6f772e6b48cb6312f384
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-25 11:07:07 +0100

    Topnav edit

--- Site/topnav.php
+++ Site/topnav.php
@@ -5,7 +5,8 @@
         <a id="closeBtn" href="#" class="close button"><img src="assets/Mewnu.png" alt="menu" class="MewShown"/></a>
         <h1> Menu</h1>
         <ul style="text-align:left;">
-            <li><a href="./">Liste des especes</a></li>
+            <li><a href="./">Acceuil</a></li>
+            <li><a href="./especes.php">Liste des especes</a></li>
             <li><a href="./attaques.php">Liste des attaques</a></li>
             <li><a href="./zones.php">Liste des zones</a></li>
             <li><a href="./types.php">Liste des types</a></li>
