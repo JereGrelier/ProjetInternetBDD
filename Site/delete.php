be50e384405738a3386c18550f637bf47dcac7ac
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-25 09:47:55 +0100

    delete.......

--- Site/delete.php
+++ Site/delete.php
@@ -5,7 +5,7 @@
     $id = intval($_REQUEST['id']);
     $table = $_REQUEST['table'];
     $tableId = $_REQUEST['tableId'];
-    $sql = 'delete from \':TABLE where :TABLEID = :id\' ';
+    $sql = 'delete from :TABLE where :TABLEID = :id ';
     $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
     $sth->execute(array('TABLE' => $table, 'TABLEID' => $tableId, 'id' => $id));
     echo $sth;
