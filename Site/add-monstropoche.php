f25390924649a6c349496152fe0371001733eebb
NAUDY Enzo <enaudy@florette.pedago.ipb.fr>
2022-03-25 12:14:59 +0100

    monstrropoche

--- Site/add-monstropoche.php
+++ Site/add-monstropoche.php
@@ -12,16 +12,15 @@
 <?php 
     include 'connect.php';
     $newnickame = $_REQUEST['Nickname'];
-    $state = 'Repos';
-    $EXP = intval('0');
     $HP = intval($_REQUEST['HP']);
     $gender = $_REQUEST['Gender'];
     $species = intval($_REQUEST['Species']);
     $object = intval($_REQUEST['Object']);
     $attack = intval($_REQUEST['Attack']);
     $owner = intval($_REQUEST['Owner']);
-    $sql = 'insert into MONSTROPOCHE (surnom, etat, pe, pv , genre, espece, objet, attaque, proprietaire) values (:surnom, :etat, :pe, :pv, :genre, :espece, :objet, :attaque, :proprietaire);';
+    $idM = 'select max(IdMonstropoche) from MONSTROPOCHE' + 1;
+    $sql = 'insert into MONSTROPOCHE (surnom, etat, pe, pv , genre, numEspece, idObjet, idProprietaire) values (:surnom, Repos, 0, :pv, :genre, :espece, :objet, :proprietaire); insert into MOVESET_MONSTROPOCHE (IdAttaque, IdMonstopoche, Position) values (:attaque, :idMonstropoche, 1);';
     $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-    $sth->execute(array('surnom'=> $newnickame, 'etat' => $state, 'pe' => $EXP, 'pv' => $HP, 'genre' => $gender, 'espece' => $species, 'objet' => $object, 'attaque' => $attack, 'proprietaire' => $owner));
+    $sth->execute(array('surnom'=> $newnickame, 'pv' => $HP, 'genre' => $gender, 'espece' => $species, 'objet' => $object, 'proprietaire' => $owner, 'attaque' => $attack, 'idMonstropoche'=>$idM));
     ?>
 </html>
