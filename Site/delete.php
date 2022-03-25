c1b60f420af8e473e84f7da33e0c5d7216469297
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-25 09:58:20 +0100

    delete..............................................................

--- Site/delete.php
+++ Site/delete.php
@@ -1,10 +1,15 @@
 
 
-<?php 
+<?php
     include 'connect.php';
     $id = intval($_REQUEST['id']);
     $table = $_REQUEST['table'];
     $tableId = $_REQUEST['tableId'];
     $sql = 'delete from ? where ? = ? ';
     $sth = $dbh->prepare($sql, array($table, $tableId, $id));
+    if ($sth->execute()) { 
+        echo ('Success');
+     } else {
+        echo('Error');
+     }
     ?>
