deafee943020135bcbb85e37eece03ed759c61ba
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-25 08:16:11 +0100

    Edit Site

--- Site/PHP/footer.php
+++
@@ -1,9 +0,0 @@
-<html lang="en">
-<body>
-    <footer>
-        <?php include 'connect.php'
-            ?>
-        Copyright Jeremy Grelier
-    </footer>
-</body>
-</html>
--- Site/js/nav.js
+++ Site/js/nav.js
@@ -1,22 +1,13 @@
 var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("menuBouton");
 var closeBtn = document.getElementById("closeBtn");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
+closeBtn.onclick = openNav;
 
-/* Set the width of the side navigation to 250px */
+
 function openNav() {
-  sidenav.classList.add("active");
-}
-
-/* Set the width of the side navigation to 0 */
-/**
- * It takes a random number from 1 to 898, and then uses that number to get a random pokemon from the
- * API. It then uses the data from the API to fill in the HTML elements
- * @returns The JSON object that is being returned is a dictionary of the pokemon's name, id, weight,
- * types, and moves.
- */
-function closeNav() {
-  sidenav.classList.remove("active");
+  if (sidenav.classList.contains("active")){
+    sidenav.classList.remove("active");
+  }
+  else
+    sidenav.classList.add("active");
 }
