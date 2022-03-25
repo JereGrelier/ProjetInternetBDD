4c7f05f0a725fe0c991e2e56d4ffb64ea529abe6
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-25 11:05:10 +0100

    searchEspece

---
+++ Site/searchEspece.php
@@ -0,0 +1,17 @@
+<?php 
+        include 'connect.php';
+        $sql = 'select * from ESPECE';
+        $res = $dbh->query($sql);
+        if($res)
+        {
+            /* ... on récupère un tableau stockant le résultat */
+                $choices = $res->fetchAll();
+                foreach($choices as $choice) {
+                echo '<option value="'.$choice['Numero'].'">'.$choice['NomEspece'].'</option>';
+                }
+                /*liberation de l'objet requete:*/
+            //$choices->closeCursor();
+            /*fermeture de la connexion avec la base*/
+            $dbh = null;
+        }
+    ?>
