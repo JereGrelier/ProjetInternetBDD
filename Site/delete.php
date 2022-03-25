c4b33011a1d1cf3cff60c87a915195272002aa49
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-25 10:09:34 +0100

    deleting.......

--- Site/delete.php
+++ Site/delete.php
@@ -7,7 +7,7 @@
     $tableId = $_REQUEST['tableId'];
     $sql = 'delete from ? where ? = ? ';
     $sth = $dbh->prepare($sql);
-    if ($sth->execute(array($table, $tableId, $id))) { 
+    if ($sth->execute(array('\''.$table, $tableId, $id.'\''))) { 
         echo ('Success');
      } else {
         echo('Error');
