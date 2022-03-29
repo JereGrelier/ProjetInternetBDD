b753ee1571691a5cf156051db35b9c0806c687f1
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-29 17:00:17 +0200

    ?

--- Site/monstropoches.php
+++ Site/monstropoches.php
@@ -28,7 +28,7 @@
         <th>PE</th>
         <th>Genre</th>
         <th>Espèce</th>
-        <th colspan="4">Attaque</th>
+        <th colspan="5">Attaque</th>
         <th>Objet</th>
         <th>Propriétaire</th>
         <th>Actions</th>
