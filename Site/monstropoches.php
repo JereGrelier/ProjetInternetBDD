2cdb1dfb98cf99b24799c92f34745978026c04b2
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-29 17:15:24 +0200

    S

--- Site/monstropoches.php
+++ Site/monstropoches.php
@@ -77,7 +77,7 @@
             $dbh = null;
         echo'
       </table>
-      <button onclick="document.getElementById(\'mydialog\').style.visibility = \'visible\'">Ajouter un Monstropoches</button>
+      <button onclick="document.getElementById(\'mydialog\').style.visibility = \'visible\'">Ajouter un Monstropoche</button>
    </div>
    <dialog open id="mydialog" class="ModalAddSpecies" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
       <h1 id="modal-heading">Ajouter un Monstropoches</h1>
