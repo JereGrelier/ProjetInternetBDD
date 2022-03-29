8344d9938998db3e6237a1d5c1cc94e85ae63aa4
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-29 14:15:44 +0200

    F*cking Favicons

--- Site/objets.php
+++ Site/objets.php
@@ -4,14 +4,14 @@
  <link rel="stylesheet" href="css/monsite.css" />
  <meta charset="utf-8"/>
   <title>Liste des objets</title>
-  <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
-        <link rel="manifest" href="/assets/icons/site.webmanifest">
-        <link rel="mask-icon" href="/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
-        <link rel="shortcut icon" href="/assets/icons/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/favicon-16x16.png">
+        <link rel="manifest" href="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/site.webmanifest">
+        <link rel="mask-icon" href="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
+        <link rel="shortcut icon" href="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/favicon.ico">
         <meta name="msapplication-TileColor" content="#da532c">
-        <meta name="msapplication-config" content="/assets/icons/browserconfig.xml">
+        <meta name="msapplication-config" content="ProjetInternetBDD/Site/ProjetsBDD/assets/icons/browserconfig.xml">
         <meta name="theme-color" content="#ffffff">
  </head>
  <body>
