cf9746acc77b9f4afb4cfb18935f908211127fc7
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-29 17:39:15 +0200

    ...

--- Site/moveset-espece.php
+++ Site/moveset-espece.php
@@ -27,7 +27,8 @@
           if($res = $dbh->query($requete))
               /* ... on récupère un tableau stockant le résultat */
                 $moveset =  $res->fetchAll();
-                echo '<h2>Moveset '.isset($moveset[0]['NomEspece'])? 'de '.$moveset[0]['NomEspece'] : 'vide'.'</h2>';
+                $nam = isset($moveset[0]['NomEspece']) ? 'de'.$moveset[0]['NomEspece'] : 'vide';
+                echo '<h2>Moveset '.$nam.'</h2>';
                 $num = intval($_REQUEST['id']);
                 echo '<table>
                 <tr class="headerListEspece">
