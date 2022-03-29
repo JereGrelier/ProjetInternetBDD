d6c43f6a75808bdb00517241f08fbeb8c138a08e
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-29 16:45:01 +0200

    test

--- Site/searchMoveset-Espece.php
+++ Site/searchMoveset-Espece.php
@@ -1,7 +1,14 @@
+<script>
+function getdata(){
+      var txtOne = document.getElementById('Species').value;
+      document.cookie = "myJavascriptVar = " + txtOne </script>
+}
+</script>
 <?php 
+        $test= $_COOKIE['myJavascriptVar'];
         include 'connect.php';
-        echo '<option value="null">'.(intval($_POST['Species'])).'</option>';
-        $sql = 'select * from MOVESET_ESPECE, ATTAQUE where NumEspece = '.$a. ' and MOVESET_ESPECE.IdAttaque = ATTAQUE.IdAttaque';
+        echo '<option value="null">'.(intval($_POST['test'])).'</option>';
+        $sql = 'select * from MOVESET_ESPECE, ATTAQUE where NumEspece = '.$test. ' and MOVESET_ESPECE.IdAttaque = ATTAQUE.IdAttaque';
         $res = $dbh->query($sql);
         if($res)
         {
