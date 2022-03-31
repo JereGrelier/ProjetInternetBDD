f33c32b3b5b338985c129e4bef366bfb3401c67e
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-31 09:36:28 +0200

    ping

--- Site/js/modal.js
+++ Site/js/modal.js
@@ -1,9 +1,10 @@
 var modal = document.getElementById('mydialog');
+var button = document.getElementsByClassName('openModal')
 
 // When the user clicks anywhere outside of the modal, close it
 
 window.onclick = function(event) {
-   if (event.target != modal) {
+   if (event.target != modal && event.target != button.item(0)) {
 
       modal.style.visibility='hidden';
 
