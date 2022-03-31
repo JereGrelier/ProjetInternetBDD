5bad015c4e8ef114c6b510b5c090841d07d60b00
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-31 14:17:16 +0200

    parent.parent.parent...

--- Site/js/modal.js
+++ Site/js/modal.js
@@ -9,7 +9,7 @@ button.onclick = function() {
 }
 
 window.onclick = function(event) {
-   if (event.target != modal && event.target != button && event.target.parentNode != modal  && event.target.parentNode.parentNode != modal) {
+   if (event.target != modal && event.target != button && event.target.parentNode != modal && event.target.parentNode.parentNode != modal && event.target.parentNode.parentNode.parentNode != modal) {
         console.log(event.target);
         console.log("On ferme");
         modal.style.visibility='hidden';
