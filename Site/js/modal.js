1e2f08006149b723f844b7fd96bb26fcd6b267e0
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-03-31 14:20:04 +0200

    parent.parent.parent.parent

--- Site/js/modal.js
+++ Site/js/modal.js
@@ -9,9 +9,8 @@ button.onclick = function() {
 }
 
 window.onclick = function(event) {
-   if (event.target != modal && event.target != button && event.target.parentNode != modal && event.target.parentNode.parentNode != modal && event.target.parentNode.parentNode.parentNode != modal) {
-        console.log(event.target.parentNode.parentNode.parentNode);
-        console.log("On ferme");
+   if (event.target != modal && event.target != button && event.target.parentNode != modal && event.target.parentNode.parentNode != modal
+     && event.target.parentNode.parentNode.parentNode != modal && event.target.parentNode.parentNode.parentNode.parentNode != modal) {
         modal.style.visibility='hidden';
     }
 }
