a9be6522661e98ae1c05ce534c189a6dbb43aa35
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-04-05 17:40:59 +0200

    ARGH

--- Site/especes.php
+++ Site/especes.php
@@ -21,26 +21,26 @@
 <body>
   <?php include "topnav.php" ?>
   <div class="listeEspece">
-   
+
   </div>
 
   <div class="TableEff">
-  <h2>Liste des espèces de monstropochetrons</h2>
-    <div id="myGrid" style="width: 100em; height: 100%;position: fixed;left: 50px;border-radius: 15px;overflow: auto;font-size: large;" class="ag-theme-alpine"></div>
+    <h2>Liste des espèces de monstropochetrons</h2>
+    <div id="myGrid" style="width: 100em; height: 130%;position: absolute;left: 50px;border-radius: 15px;overflow: auto;font-size: large;" class="ag-theme-alpine"></div>
   </div>
   <script type="text/javascript">
     <?php
-  include "connect.php";
-  $requete = "select * from ESPECE, TYPE, HABITAT, ZONE where ESPECE.TypeEspece = TYPE.idType and ESPECE.Numero = HABITAT.NumEspece and HABITAT.IdZone = ZONE.IdZone order by ESPECE.numero asc";
-  /* Si l'execution est reussie... */
-  if ($res = $dbh->query($requete))
-    /* ... on récupère un tableau stockant le résultat */
-    $espece =  $res->fetchAll();
-  /*liberation de l'objet requete:*/
-  $res->closeCursor();
-  /*fermeture de la connexion avec la base*/
-  $dbh = null;
-  ?>
+    include "connect.php";
+    $requete = "select * from ESPECE, TYPE, HABITAT, ZONE where ESPECE.TypeEspece = TYPE.idType and ESPECE.Numero = HABITAT.NumEspece and HABITAT.IdZone = ZONE.IdZone order by ESPECE.numero asc";
+    /* Si l'execution est reussie... */
+    if ($res = $dbh->query($requete))
+      /* ... on récupère un tableau stockant le résultat */
+      $espece =  $res->fetchAll();
+    /*liberation de l'objet requete:*/
+    $res->closeCursor();
+    /*fermeture de la connexion avec la base*/
+    $dbh = null;
+    ?>
     var esp = <?php echo json_encode($espece); ?>;
 
     var columnDefs = [{
@@ -78,21 +78,23 @@
         headerName: "Image",
         field: 'img',
         resizable: true,
+        sortable: false,
         cellRenderer: function(params) {
-                // Display the image
-                /* This is a function that will display the image of the species. */
-                let link = `<img src=${params.value} alt=${params.value} />`;
-                return link;
-            }
+          // Display the image
+          /* This is a function that will display the image of the species. */
+          let link = `<img src=${params.value} alt=${params.value} />`;
+          return link;
+        }
       },
       {
         headerName: "Action",
         field: 'Action',
         resizable: true,
+        sortable: false,
         cellRenderer: function(params) {
-                // Display the image
-                /* This is a function that will display the image of the species. */
-                let link = `<form method="post" action="delete/deleteEspece.php">
+          // Display the image
+          /* This is a function that will display the image of the species. */
+          let link = `<form method="post" action="delete/deleteEspece.php">
                       <button type="submit" name="btnEnvoiForm" title="Envoyer"><h2 style="color:black">Supprimer</h2></button>
                       <input type="hidden" name="id" value=${params.value}/>
                     </form>
@@ -100,8 +102,8 @@
                   <button type="submit" title="Envoyer"><h2 style="color:black">Voir le moveset</h2></button>
                   <input type="hidden" name="id" value=${params.value}/>
                   </form>`;
-                return link;
-            }
+          return link;
+        }
       },
     ];
 
@@ -123,8 +125,11 @@
     var gridOptions = {
       defaultColDef: {
         sortable: true,
-        cellStyle: {fontSize: '22px', textAlign: 'center'}
-    },
+        cellStyle: {
+          fontSize: '22px',
+          textAlign: 'center'
+        }
+      },
       columnDefs: columnDefs,
       rowData: rowData,
       rowHeight: 130,
@@ -138,29 +143,29 @@
     });
   </script>
   <button id="openModal"> Ajouter une espèce</button>
-    <div id="mydialog">
-      <dialog open class="ModalAddSpecies" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
-        <h1 id="modal-heading">Ajouter une Espèce</h1>
-        <form action="./add/add-espece.php" method="post">
-          <label>Numéro : <input type="number" id="number" name="number" min=1 required><br></label>
-          <label>Nom : <input type="text" id="name" name="name" required><br></label>
-          <label>Type : <select name="type" id="type" required>
-              <option value="">--Choisissez--</option>
-              <?php include "./search/searchType.php" ?>
-            </select><br> </label>
-          <label>Type 2 : <select name="type-deux" id="type-deux">
-              <option value=null>--Choisissez--</option>
-              <?php include "./search/searchType.php" ?>
-            </select><br> </label>
-          <label>Zone : <select name="zone" id="zone" required>
-              <option value="">--Choisissez--</option>
-              <?php include "./search/searchZone.php" ?>
-            </select> <br></label>
-          <input type="submit" value="valider">
-        </form>
-        <button onclick="document.getElementById('mydialog').style.visibility='hidden'" style="position: inherit;top: -4px;left: 80%;border: none;background: transparent;"><img src="assets/376.png" alt="close" style="width: 60px; height: 60px;" /></button>
-      </dialog>
-    </div>
+  <div id="mydialog">
+    <dialog open class="ModalAddSpecies" role="dialog" aria-modal="true" aria-labelledby="modal-heading">
+      <h1 id="modal-heading">Ajouter une Espèce</h1>
+      <form action="./add/add-espece.php" method="post">
+        <label>Numéro : <input type="number" id="number" name="number" min=1 required><br></label>
+        <label>Nom : <input type="text" id="name" name="name" required><br></label>
+        <label>Type : <select name="type" id="type" required>
+            <option value="">--Choisissez--</option>
+            <?php include ("../php/search/searchType.php") ?>
+          </select><br> </label>
+        <label>Type 2 : <select name="type-deux" id="type-deux">
+            <option value=null>--Choisissez--</option>
+            <?php include ("../php/search/searchType.php") ?>
+          </select><br> </label>
+        <label>Zone : <select name="zone" id="zone" required>
+            <option value="">--Choisissez--</option>
+            <?php include ("../php/search/searchType.php") ?>
+          </select> <br></label>
+        <input type="submit" value="valider">
+      </form>
+      <button onclick="document.getElementById('mydialog').style.visibility='hidden'" style="position: inherit;top: -4px;left: 80%;border: none;background: transparent;"><img src="assets/376.png" alt="close" style="width: 60px; height: 60px;" /></button>
+    </dialog>
+  </div>
   <script type="text/javascript" src="./js/modal.js"></script>
   <?php include "footer.php" ?>
 </body>
--- Site/search/searchType.php
+++ Site/search/searchType.php
@@ -1,16 +1,20 @@
 <?php 
         include '../connect.php';
         $sql = 'select * from TYPE';
+
+        echo 'Allhuile ?';
         $res = $dbh->query($sql);
         if($res)
         {
             /* ... on récupère un tableau stockant le résultat */
                 $choices = $res->fetchAll();
+                echo 'Allo ?';
                 foreach($choices as $choice) {
                 echo '<option value="'.$choice['IdType'].'">'.$choice['NomType'].'</option>';
                 }
                 /*liberation de l'objet requete:*/
-            //$choices->closeCursor();
+                print_r($choices);
+            $res->closeCursor();
             /*fermeture de la connexion avec la base*/
             $dbh = null;
         }
