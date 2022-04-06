4c1126cfefa011e64b05542556b379f01f21156c
Jérémy Grelier <jeremy.grelier33@gmail.com>
2022-04-06 08:47:17 +0200

    Add-monstropoche

--- Site/add/add-monstropoche.php
+++ Site/add/add-monstropoche.php
@@ -35,23 +35,37 @@ $attackee = $_REQUEST['Attackee'] == "null" ? NULL : $_REQUEST['Attackee'];
 $attackeee = $_REQUEST['Attackeee'] == "null" ? NULL : $_REQUEST['Attackeee'];
 $owner = $_REQUEST['Owner'] == "null" ? NULL : intval($_REQUEST['Owner']);
 $sql = 'insert into MONSTROPOCHE (surnom, etat, pe, pv , genre, numEspece, idObjet, idProprietaire) values (:surnom, :state, :exp, :pv, :genre, :espece, :objet, :proprietaire);';
-$sql2 = 'insert into MOVESET_MONSTROPOCHE (IdAttaque, idMonstropoche, Position) values (:attaque, :idMonstropoche, 1), (:attaquee, :idMonstropoche, 2), (:attaqueee, :idMonstropoche, 3), (:attaqueeee, :idMonstropoche, 4);';
 $sth = $dbh->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 $sth->execute(array(
     'surnom' => $newnickame, 'pv' => $HP, 'genre' => $gender, 'exp' => $EXP, 'state' => $state,
     'espece' => $species, 'objet' => $object, 'proprietaire' => $owner
 ));
-$requete1 = "select * from MONSTROPOCHE where MONSTROPOCHE.Surnom = '".$newnickame."' and MONSTROPOCHE.NumEspece = '". $species."'";
+$requete1 = "select * from MONSTROPOCHE where MONSTROPOCHE.Surnom = '" . $newnickame . "' and MONSTROPOCHE.NumEspece = '" . $species . "'";
 /* Si l'execution est reussie... */
 if ($res1 = $dbh->query($requete1))
-  /* ... on récupère un tableau stockant le résultat */
-  $monstropoches =  $res1->fetchAll();
-  echo $monstropoches[0]['IdMonstropoche'];
-$sth2 = $dbh->prepare($sql2, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
-$sth2->execute(array(
-    'attaque' => $attack, 'attaquee' => $attacke,
-    'attaqueee' => $attackee, 'attaqueeee' => $attackeee, 'idMonstropoche' => $monstropoches[0]['IdMonstropoche']
-));
+    /* ... on récupère un tableau stockant le résultat */
+    $monstropoches =  $res1->fetchAll();
+echo $monstropoches[0]['IdMonstropoche'];
+$sqlA1 = 'insert into MOVESET_MONSTROPOCHE (IdAttaque, idMonstropoche, Position) values (:attaque, :idMonstropoche, 1);';
+$sthA1 = $dbh->prepare($sqlA1, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+$sthA1->execute(array('attaque' => $attack, 'idMonstropoche' => $monstropoches[0]['IdMonstropoche']));
+
+//Gestion des attaques multiples
+if ($attacke) {
+    $sqlA2 = 'insert into MOVESET_MONSTROPOCHE (IdAttaque, idMonstropoche, Position) values (:attaquee, :idMonstropoche, 2);';
+    if ($attackee) {
+        $sqlA3 = 'insert into MOVESET_MONSTROPOCHE (IdAttaque, idMonstropoche, Position) values (:attaqueee, :idMonstropoche, 3);';
+        if ($attackeee) {
+            $sqlA4 = 'insert into MOVESET_MONSTROPOCHE (IdAttaque, idMonstropoche, Position) values (:attaqueeee, :idMonstropoche, 4);';
+            $sthA4 = $dbh->prepare($sqlA4, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+            $sthA4->execute(array('attaqueeee' => $attackeee, 'idMonstropoche' => $monstropoches[0]['IdMonstropoche']));
+        }
+        $sthA3 = $dbh->prepare($sqlA3, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+        $sthA3->execute(array('attaqueee' => $attackee, 'idMonstropoche' => $monstropoches[0]['IdMonstropoche']));
+    }
+    $sthA2 = $dbh->prepare($sqlA2, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $sthA2->execute(array('attaquee' => $attacke, 'idMonstropoche' => $monstropoches[0]['IdMonstropoche']));
+}
 ?>
 
 </html>
